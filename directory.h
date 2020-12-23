74b9918ce677148074a7a8978a0b6025d8fd8d18
crista <1228335053@qq.com>
2020-12-23 15:56:38 +0800

    add 1 file

---
+++ directory.h
@@ -0,0 +1,54 @@
+#ifndef DIRECTORY_H
+#define DIRECTORY_H
+
+#include "file.h"
+
+#include <string>
+#include <vector>
+
+const std::string parentDirEntryName = "..";
+const std::string currentDirEntryName = ".";
+
+struct directoryEntry
+{
+    bool isInUse = true;
+    std::int8_t reversed = 0;
+    char name[250] = {0};
+    blockNum_t blockNum;
+
+    directoryEntry(std::string name, blockNum_t blockNum);
+    directoryEntry() {}
+};
+
+constexpr size_t directoryEntrySize = 256;
+static_assert(sizeof(directoryEntry) == directoryEntrySize);
+
+class directory
+{
+  private:
+    file _file;
+
+  public:
+    directory(file f) : _file(f) {}
+    file &dirFile() { return _file; }
+
+    static directory create(blockNum_t blockNum);
+    static directory open(blockNum_t blockNum);
+    void addEntry(directoryEntry entry);
+
+    blockNum_t findEntry(std::string name, std::streampos &foundPosition);
+    blockNum_t findEntry(std::string name);
+    std::string findEntryName(blockNum_t);
+    std::vector<directoryEntry> allEntries();
+    void removeEntry(std::streampos foundPosition);
+};
+
+class not_a_directory : public std::runtime_error
+{
+    using base = std::runtime_error;
+
+  public:
+    not_a_directory() : base("Trying to open a file that is not directory.") {}
+};
+
+#endif 
---
+++ mode_change.h
@@ -0,0 +1,89 @@
+#ifndef MODE_CHANGE_H
+#define MODE_CHANGE_H
+
+#include <memory>
+#include <deque>
+#include <map>
+#include <string>
+
+#include "file.h"
+
+class changeTarget
+{
+  public:
+    virtual permission &targetPermission(filePermission &) = 0;
+
+    static const std::map<char, std::reference_wrapper<changeTarget>> all;
+};
+class userTarget : public changeTarget
+{
+  public:
+    permission &targetPermission(filePermission &p) override { return p.user(); }
+
+    static userTarget instance;
+};
+class otherTarget : public changeTarget
+{
+  public:
+    permission &targetPermission(filePermission &p) override { return p.other(); }
+
+    static otherTarget instance;
+};
+
+class modeChange
+{
+  public:
+    using targets_t = std::deque<std::reference_wrapper<changeTarget>>;
+    using permissions_t = std::deque<std::reference_wrapper<permissionAccessor>>;
+    using operation_ptr = std::reference_wrapper<modeChangeOperation>;
+
+  private:
+    targets_t targets;
+    permissions_t permissions;
+    const operation_ptr operation;
+
+  public:
+    modeChange(targets_t& ts, permissions_t& ps, const operation_ptr &po)
+        : targets(ts), permissions(ps), operation(po) {}
+    void apply(filePermission &);
+};
+
+class modeChangeOperation
+{
+  public:
+    virtual void apply(permission &, modeChange::permissions_t&) = 0;
+    static const std::map<char, std::reference_wrapper<modeChangeOperation>> all;
+};
+
+class addChangeOperation : public modeChangeOperation
+{
+  public:
+    void apply(permission &, modeChange::permissions_t&) override;
+    static addChangeOperation instance;
+};
+
+class removeChangeOperation : public modeChangeOperation
+{
+  public:
+    void apply(permission &, modeChange::permissions_t&) override;
+    static removeChangeOperation instance;
+};
+
+class setChangeOperation : public modeChangeOperation
+{
+  public:
+    void apply(permission &, modeChange::permissions_t&) override;
+    static setChangeOperation instance;
+};
+
+class modeChanges
+{
+  private:
+    std::deque<modeChange> changes;
+
+  public:
+    modeChanges(std::string);
+    void apply(filePermission &);
+};
+
+#endif
